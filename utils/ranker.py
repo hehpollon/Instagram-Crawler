00c64d41032e664ae5f46bdda9155fdd05c20af3
hehpollon <hehpollon@gmail.com>
2018-06-14 00:07:34 +0900

    add upper and lower

--- utils/ranker.py
+++ utils/ranker.py
@@ -13,33 +13,40 @@ def makeDir(dirPath):
 def main():
     args = docopt("""
     Usage:
-        ranker.py [-q QUERY] [-n NUMBER] [-h HELP]
+        ranker.py [-q QUERY] [-u upper] [-l lower] [-f FOLDER]
     
     Options:
         -q QUERY  username, add '#' to search for hashtags, e.g. 'username' or '#hashtag'
                   For multiple query seperate with comma, e.g. 'username1, username2, #hashtag'
 
-        -n NUM    number of split likes
+        -u upper  upper number of likes
 
-        -h HELP   show this help message and exit
+        -l lower  lower number of likes
+
+        -f FOLDER name of folder
+
     """)
     makeDir("../s_data")
-    limitNum = int(args.get('-n', 0))
+    highNum = int(args.get('-u', "10000"))
+    lowNum = int(args.get('-l', "0"))
     query = args.get('-q', "")
-    if not query or limitNum == 0:
+    folderName = args.get('-f', "")
+    print(lowNum, "<=", folderName, "<=", highNum)
+    newPath = "../s_data/"+query+"/"+folderName
+    if not query or highNum <= lowNum or not folderName:
         print('Please input query!')
     else:
-        if os.path.exists("../s_data/"+query):
+        if os.path.exists(newPath):
             # remove exist folder
-            shutil.rmtree("../s_data/"+query)
+            shutil.rmtree(newPath)
 
         # make new folder
         makeDir("../s_data/"+query)
-        makeDir("../s_data/"+query+"/high")
-        makeDir("../s_data/"+query+"/low")
+        makeDir(newPath)
 
         path = "../data/"+query
-
+        count = 0
+        totalCount = 0
         for i in os.listdir(path):
             try:
                 fPath = path+"/"+i+"/info.txt"
@@ -49,16 +56,16 @@ def main():
                 f.readline()
                 data = f.readline()[:-1]
                 likes = int(data)
-                if likes >= limitNum:
-                    shutil.copytree(path+"/"+i, "../s_data/"+query+"/high/"+i)
-                else:
-                    shutil.copytree(path+"/"+i, "../s_data/"+query+"/low/"+i)
+                totalCount += 1
+                if likes <= highNum and likes >= lowNum:
+                    shutil.copytree(path+"/"+i, newPath+"/"+i)
+                    count += 1
             except:
                 pass
             finally:
                 f.close()
 
+    print(str(count)+"/"+str(totalCount)+" matched!")
 main()
-
 
 
